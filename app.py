b2a472da4169f1335a6c992054d3de1a458f0b32
alexwaitzman <148055964+alexwaitzman@users.noreply.github.com>
2023-11-06 17:55:15 +0200

    Corrected final version

--- app.py
+++ app.py
@@ -2,17 +2,35 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 #creating a header
-st.header('Reading info on our dataset')
+st.header('Investigation of car advertisement market in US')
+#creating a project description
+st.write('In this app we will investigate dependancy of the car price from a condition and from a model')
 #creating a subheader
 st.write("""
 ### Head of data
 """)
 #reading the file
 df=pd.read_csv('vehicles_us.csv')
+
+#Filling in missing values in the data
+df.isna().sum()
+
+# looping over column names and replacing missing values with 'unknown'
+columns_to_replace = ['model_year','cylinders','odometer','paint_color','is_4wd']
+for column in columns_to_replace:
+   df[column] = df[column].fillna('unknown')
+
+#checking for duplicates
+df.duplicated().sum()
+#droping duplicates
+df=df.drop_duplicates()
+#checking for duplicates again
+df.duplicated().sum()
+
 st.table(df.head())
 
 #creating slider for the price range and checkbox for excellent condition
-st.caption(':red[Choose your parameters here]')
+st.caption('Choose your parameters here')
 price_range = st.slider(
      "What is your price range?",
      value=(1, 375000))
@@ -22,7 +40,6 @@ actual_range=list(range(price_range[0],price_range[1]+1))
 excellent_condition = st.checkbox('only excellent condition')
 
 if excellent_condition:
-    filtered_data=df[df.price.isin(actual_range)]
     filtered_data=filtered_data[df.condition=='excellent']
 else:
     filtered_data=df[df.price.isin(actual_range)]
@@ -37,3 +54,8 @@ st.plotly_chart(fig)
 st.write('Average price by model')
 fig2 = px.histogram(filtered_data, x="model", y="price",histfunc="avg")
 st.plotly_chart(fig2)
+
+st.header('Conclusion')
+st.write('We found out that:')
+st.write('1) The prices of the cars with new, excellent, like new and good conditions are mostly before 100K')
+st.write('2) The average prices for most models are below 10 000 $')
